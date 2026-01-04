b22bb513ad50001f299b0bc85e9f343e3ff1684d
AhmedAbdElnaby <ahmed.abd.elnaby@outlook.com>
2026-01-04 04:28:45 +0200

    feat(DC-005): Add Eureka server related changes

--- src/main/java/com/fitrah/dento/gatewayserver/config/GatewayConfig.java
+++ src/main/java/com/fitrah/dento/gatewayserver/config/GatewayConfig.java
@@ -11,14 +11,14 @@ public class GatewayConfig {
     @Bean
     public RouteLocator dentoGatewayRoutes(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
-            .route("auth-service", r -> r.path("/auth/**").uri("http://localhost:8081"))
-            .route("patient-service", r -> r.path("/patients/**").uri("http://localhost:8082"))
-            .route("dentist-service", r -> r.path("/dentists/**").uri("http://localhost:8083"))
-            .route("appointment-service", r -> r.path("/appointments/**").uri("http://localhost:8084"))
-            .route("treatment-service", r -> r.path("/treatments/**").uri("http://localhost:8085"))
-            .route("billing-service", r -> r.path("/billing/**").uri("http://localhost:8086"))
-            .route("notification-service", r -> r.path("/notifications/**").uri("http://localhost:8087"))
-            .route("reporting-service", r -> r.path("/reports/**").uri("http://localhost:8088"))
+            .route("auth-service", r -> r.path("/auth/**").uri("lb://auth-service")) // use load balancing from Eureka
+            .route("patient-service", r -> r.path("/patients/**").uri("lb://patient-service"))
+            .route("dentist-service", r -> r.path("/dentists/**").uri("lb://dentist-service"))
+            .route("appointment-service", r -> r.path("/appointments/**").uri("lb://appointment-service"))
+            .route("treatment-service", r -> r.path("/treatments/**").uri("lb://treatment-service"))
+            .route("billing-service", r -> r.path("/billing/**").uri("lb://billing-service"))
+            .route("notification-service", r -> r.path("/notifications/**").uri("lb://notification-service"))
+            .route("reporting-service", r -> r.path("/reports/**").uri("lb://reporting-service"))
             .build();
     }
 }
