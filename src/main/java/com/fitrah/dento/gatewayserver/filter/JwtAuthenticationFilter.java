a7ebec4487224f21806a6e9b8979ff126d3a9fed
AhmedAbdElnaby <ahmed.abd.elnaby@outlook.com>
2026-01-11 01:46:02 +0200

    feat(DC-008): User, Role & Authorization Model

--- src/main/java/com/fitrah/dento/gatewayserver/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/fitrah/dento/gatewayserver/filter/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.fitrah.dento.gatewayserver.filter;
 
 import com.fitrah.dento.dento_security_util.util.JwtUtil;
+import io.jsonwebtoken.Claims;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -11,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Slf4j
 @Component
@@ -37,7 +40,18 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
             return exchange.getResponse().setComplete();
         }
 
+
         var token = authHeader.substring(7); // to exclude 'Bearer' word from the token
+
+        Claims claims = jwtUtil.extractClaims(token);
+
+        String username = claims.getSubject();
+        List<String> roles = claims.get("roles", List.class);
+
+        // TEMPORARY: log to verify
+        log.info("Authenticated user: {}", username);
+        log.info("Roles: {}", roles);
+
         try {
             jwtUtil.extractUsername(token);
         } catch (Exception _) {
